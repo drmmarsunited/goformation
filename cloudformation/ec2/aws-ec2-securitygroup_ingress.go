a6733843c5b7ece6ac8e2183788d208c3b2518f7
Mitch Robins <mtchelr@amazon.com>
2023-07-11 09:47:35 -0500

    changing securitygroupingress ipprotocol to generic

--- cloudformation/ec2/aws-ec2-securitygroup_ingress.go
+++ cloudformation/ec2/aws-ec2-securitygroup_ingress.go
@@ -33,7 +33,7 @@ type SecurityGroup_Ingress[T any] struct {
 	// IpProtocol AWS CloudFormation Property
 	// Required: true
 	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-security-group-rule.html#cfn-ec2-security-group-rule-ipprotocol
-	IpProtocol string `json:"IpProtocol"`
+	IpProtocol T `json:"IpProtocol"`
 
 	// SourcePrefixListId AWS CloudFormation Property
 	// Required: false
